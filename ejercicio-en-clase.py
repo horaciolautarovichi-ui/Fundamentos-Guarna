516ae74fb4509a5352deb86668261e0d933368fe
Lautaro <horaciolautarovichi@gmail.com>
2026-09-09 19:57:11 -0300

    primer push

---
+++ ejercicio-en-clase.py
@@ -0,0 +1,42 @@
+
+
+def ingresar_lista():
+
+    lista = []
+
+    num = int(input("Ingresa valores a la lista (escribe 0 para terminar):"))
+
+    while num != 0:
+        lista.append(num)
+        num = int(input("Ingresa valores a la lista (escribe 0 para terminar):"))
+
+    print("mostrar lista")
+
+
+def mostrar_lista(lista):
+    for valores in lista:
+        print(valores)
+
+
+def num_pares(lista):
+    pares = 0
+    i = 0
+    tercer_par = 0
+    while pares < 3 and i < len(lista):
+        if lista[i] % 2 == 0:
+            pares = pares + 1
+        i = i + 1    
+
+    if pares == 3:
+        tercer_par = lista[i-1]  
+        print(f"el tercer numero par es {tercer_par}")
+    else:
+        print(lista)
+
+def main():
+    lista =  ingresar_lista()
+   
+    mostrar_lista(lista)
+    num_pares(lista)
+
+main()
---
+++ primer-parcial/parcial-1.py
@@ -0,0 +1,19 @@
+##1- Escribir en Python una función que reciba una matriz (lista de listas) de enteros y devuelva True si esa matriz es cuadrada (las filas y las columnas tienen que ser ##de la misma longitud), False de lo contrario.
+
+##2-Escribir en Python una función que reciba una matriz cuadrada (lista de listas) de enteros y devuelva True si esa matriz es diagonal superior (los elementos que están ##debajo de la diagonal principal, la que va de 1,1 a n,n, deben ser False de lo contrario.
+                                                                                                                                                 
+##3-Escribir en Python una función que reciba una matriz cuadrada (lista de listas) de enteros y devuelva True si esa matriz es la identidad, (los de la diagonal ##principal, la que va de 1,1 a n,n, deben ser 1 y todos los demás, 0, False de lo contrario.
+                                                                                                                                             
+##4-Escribir en Python una función que reciba una matriz (lista de listas) de enteros y devuelva una lista en donde el elemento i sea la suma de la fila i de la matriz.
+##5-Escribir en Python una función que reciba una matriz (lista de listas) de enteros y devuelva una lista en donde el elemento i sea la suma de la columna i de la matriz.
+
+def ejercicio1(M):
+   filas = len(M)
+   columnas = len(M[0])
+   cuadrada = False
+   if filas == columnas:
+      cuadrada = True
+   return cuadrada
+
+   
+     
