b055da97c2326851afae7a7c3a9c0c60fada8a91
Lautaro <horaciolautarovichi@gmail.com>
2026-09-09 22:49:20 -0300

    primer ejercicio del parcial A terminado

---
+++ primer-parcial/parcialA.py
@@ -0,0 +1,119 @@
+"""
+En los ejercicios que debas utilizar ciclos, la incorrecta elección del ciclo o el uso incorrecto del mismo, en base a lo explicado en las clases, invalidará el ejercicio por completo.
+
+No se pueden utilizar métodos y/o recursos no explicados en el curso.
+
+En todos los ejercicios se debe cumplir con las prácticas que se explicaron en el curso (nombres, estilos, etc.) y las que se encuentran en el PEP de la cátedra. Además se debe prestar especial atención a la modularización y la eficiencia (no hacer ciclos de más, etc.).
+
+Escribir con letra clara y poner nombre y legajo a todas las hojas."""
+
+
+
+
+
+
+""" 
+
+Ejercicio 1
+
+Escribir una función que reciba una cadena de caracteres que representa un alias bancario. La función deberá devolver True o False, en base a haber evaluado que dicho alias esté bien formado.
+
+Se debe controlar:
+a. Que tenga entre 6 y 20 caracteres, pudiendo ser letras, números y caracteres especiales (guión del medio, guión bajo o punto).
+
+b. Que contenga al menos 6 letras y un carácter especial.
+
+c. Que los caracteres especiales no se encuentren ni en la primera, ni la última posición.
+
+RESTRICCIÓN: De los métodos de la clase cadena, sólo se puede usar el método isalpha. Se deben evitar ciclos innecesarios."""
+
+
+def alias_bancario(alias):
+
+    valido = False
+
+    primer_car = alias[0]
+    ultimo_car = alias[len(alias)-1]
+
+    if len(alias) > 20 or len(alias) < 6:
+        valido = False
+    else:
+        if primer_car in "._-" or ultimo_car in "_-.":
+            valido = False
+
+
+    cant_letras = 0
+    caracter_especial = 0
+
+    for caracter in alias:
+        if caracter.isalpha():
+            cant_letras += 1
+        elif caracter in ".-_":
+            caracter_especial += 1
+        elif caracter < "0" or caracter > "9":
+            valido = False
+
+    if cant_letras >= 6 and caracter_especial >= 1:
+        valido = True
+
+    return valido 
+
+
+
+""" 
+
+Ejercicio 2
+
+Escribir una función elegir_comidas en Python que reciba una lista de listas, cada sublista es una comida, en donde el primer elemento es el nombre de la comida y los siguientes elementos los respectivos ingredientes. También recibe una lista de ingredientes prohibidos. Debe devolver una lista con los nombres de las comidas permitidas (no deben contener ingredientes prohibidos).
+
+Ejemplo:
+
+Python
+comidas = [
+    ["milanesa", "bifes de nalga", "pan rallado", "huevo"],
+    ["ravioles", "harina", "espinaca", "ricota"],
+    ["pizza", "queso", "harina", "tomate", "aceitunas"],
+]
+prohibidos_1 = ["huevo", "nueces", "aceitunas"]
+prohibidos_2 = ["huevo", "nueces"]
+
+elegir_comidas(comidas, prohibidos_1)  # --> ['ravioles']
+elegir_comidas(comidas, prohibidos_2)  # --> ['ravioles', 'pizza']
+Testea la función usando doctest, para 2 listas de comidas de al menos 4 comidas cada una; y 3 listas de ingredientes prohibidos, con al menos 2 ingredientes cada una. Generá 4 casos de prueba diferentes."""
+
+
+def elegir_comidas(comidas, prohibidos):
+
+    permitidas = []
+
+    for comida in comidas:
+
+        nombre = comidas[0]
+
+        ingredientes = comidas[1:]
+
+        es_permitida = True
+
+        i = 0
+
+        while 
+
+
+
+
+
+"""
+Ejercicio 3
+
+Se cuenta con una lista salarios, ya cargada, que contiene sublistas, cada una de esas sublistas tiene los siguientes valores: empresa (string), puesto (string), cantidad_de_profesionales (entero), salario_por_profesional (entero en dólares).
+
+Ejemplo:
+
+Python
+[["GlobalTek", "Desarrollador", 55, 1800], ["GlobalTek", "Tester", 75, 1300], ["ITSol", "Desarrollador", 44, 1500], ...]
+Se pide que escribas un programa modular (compuesto por funciones) en Python que:
+
+Procese esa lista y genere un diccionario con clave: puesto y valores: total_cantidad, total_salarios. La lista con los valores a procesar se obtiene invocando a la función obtener_lista_salarios() de la librería salarios.
+
+Luego, debe mostrar por pantalla, los puestos y su promedio salarial, ordenados de mayor a menor por promedios salariales. La salida debe estar formateada, de forma que se visualice una columna para los puestos, y otra para el promedio salarial.
+"""
