a791f2ab3d2679e5a3828a388cfea71da4388fd7
Lautaro <horaciolautarovichi@gmail.com>
2026-09-12 15:54:00 -0300

    resulto ejercicio 2 parcial A y resulto la tarea para entregar

--- 114574.py
+++ 114574.py
@@ -1,7 +1,7 @@
 import doctest
 
-def evaluar_perfecto(n):
 
+def evaluar_perfecto(n):
     """
     >>> evaluar_perfecto(114574)
     False
@@ -17,18 +17,18 @@ def evaluar_perfecto(n):
 
     suma_divisores = 0
     i = 1
-    if n >= 1:
-    
-        while i < n and suma_divisores < n :
-            # Continuamos mientras no hayamos llegado al final de los posibles divisores
-            # y mientras la suma de los divisores todavía sea menor que n
+
+    if n >= 1: ##un num negativo no puede ser un num perfecto
+       
+        while i <= n // 2:
             if n % i == 0:
                 suma_divisores += i
             i = i + 1
+
         if suma_divisores == n:
             perfecto = True
-        
-    return perfecto     
+
+    return perfecto
 
 
 def main():
--- primer-parcial/parcialA.py
+++ primer-parcial/parcialA.py
@@ -8,7 +8,7 @@ En todos los ejercicios se debe cumplir con las prácticas que se explicaron en 
 Escribir con letra clara y poner nombre y legajo a todas las hojas."""
 
 
-
+import doctest
 
 
 
@@ -82,24 +82,74 @@ elegir_comidas(comidas, prohibidos_2)  # --> ['ravioles', 'pizza']
 Testea la función usando doctest, para 2 listas de comidas de al menos 4 comidas cada una; y 3 listas de ingredientes prohibidos, con al menos 2 ingredientes cada una. Generá 4 casos de prueba diferentes."""
 
 
-def elegir_comidas(comidas, prohibidos):
+def elegir_comidas(comidas : list, prohibidos:  list) -> list:
+
+    # Modulo doctest para verificar los casos de prueba requeridos por la consigna
+    """
+    >>> comidas_1 = [
+    ...     ["milanesa", "bifes de nalga", "pan rallado", "huevo"],
+    ...     ["ravioles", "harina", "espinaca", "ricota"],
+    ...     ["pizza", "queso", "harina", "tomate", "aceitunas"],
+    ...     ["ensalada", "lechuga", "tomate", "aceite"]
+    ... ]
+
+    >>> comidas_2 = [
+    ...     ["tarta", "harina", "acelga", "huevo"],
+    ...     ["sopa", "agua", "verduras", "sal"],
+    ...     ["pastel de papa", "papa", "carne", "cebolla", "huevo"],
+    ...     ["empanadas", "harina", "carne", "aceitunas"]
+    ... ]
+
+    >>> prohibidos_1 = ["huevo", "nueces", "aceitunas"]
+    >>> prohibidos_2 = ["carne", "sal"]
+    >>> prohibidos_3 = ["azucar", "mani"]
+
+    Caso 1: Usando comidas_1 con prohibidos_1
+    >>> elegir_comidas(comidas_1, prohibidos_1)
+    ['ravioles']
+
+    Caso 2: Usando comidas_1 con prohibidos_2
+    >>> elegir_comidas(comidas_1, prohibidos_2)
+    ['milanesa', 'ravioles', 'pizza', 'ensalada']
+
+    Caso 3: Usando comidas_2 con prohibidos_1
+    >>> elegir_comidas(comidas_2, prohibidos_1)
+    ['sopa']
+
+    Caso 4: Usando comidas_2 con prohibidos_2
+    >>> elegir_comidas(comidas_2, prohibidos_2)
+    ['tarta']
+    """ 
 
     permitidas = []
 
-    for comida in comidas:
+    for comida in comidas: ## recorremos cada fila de comidas en comida
 
-        nombre = comidas[0]
+        nombre = comida[0]
 
-        ingredientes = comidas[1:]
+        ingredientes = comida[1:] ## lo usamos para recorrer desde el indice uno hasta el final
 
         es_permitida = True
 
         i = 0
 
-        while 
+        while i < len(ingredientes) and es_permitida: ## mientras i sea menor a la cantidad de ingredientes y es_permitida == True seguimos
+
+            if ingredientes[i] in prohibidos: ## si en algun momento el ingrediente esta en prohibidos cortamos el bucle y pasamos a la otra comida del for
+                es_permitida = False
+            i += 1
+
+        if es_permitida == True:
+            permitidas.append(nombre )          
+
+    return permitidas
 
 
 
+def main():
+
+    # Ejecuta automáticamente las pruebas especificadas en la docstring
+    print(doctest.testmod)
 
 
 """
