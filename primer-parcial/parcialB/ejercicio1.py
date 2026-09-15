07a30015f16aa51d5307dc09a31493fc2dec5fa0
Lautaro <horaciolautarovichi@gmail.com>
2026-09-15 14:26:13 -0300

    edit

---
+++ primer-parcial/parcialB/ejercicio1.py
@@ -0,0 +1,18 @@
+# ==========================================
+# Ejercicio 1
+# ==========================================
+
+
+"""
+Escribir una función contar_caracteres_distintos que reciba una cadena de caracteres y devuelva un entero con la
+cantidad de caracteres alfabéticos diferentes sin distinguir mayúsculas de minúsculas ni vocales con tilde. Los dígitos y
+símbolos, se me descartan. Agregar las pruebas utilizando la librería doctest con los siguientes ejemplos:
+
+contar_caracteres_distintos("Aaaáb")         devuelve: 2 porque cuenta la "a" y la "b"
+contar_caracteres_distintos("Aprobé-con-7")  devuelve: 8 porque cuenta A-p-r-o-b-e-c-n
+contar_caracteres_distintos("123$-1")        devuelve: 0 porque no hay caracteres alfabéticos
+contar_caracteres_distintos("AmMimmo8")      devuelve: 4 porque cuenta A-m-i-o
+"""
+
+
+
